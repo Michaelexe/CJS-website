4ed6d388c2cb9732b024e3ef5f2316d287d5deb7
michaelexe <michaelispahani@gmail.com>
2024-10-31 14:30:08 -0400

    improved form, changed social links

--- js/form.js
+++ js/form.js
@@ -1,8 +1,10 @@
 const form = document.querySelector("form");
+submitButton = form.querySelector("button");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
+  submitButton.disabled = true;
+  submitButton.innerText = "Sending...";
   const request = new XMLHttpRequest();
   request.open(
     "post",
@@ -15,8 +17,11 @@ form.addEventListener("submit", (e) => {
     if (JSON.parse(request.response).result === "success") {
       alert("Form sent successfully!");
       form.reset();
+      submitButton.innerText = "Submitted!";
     } else {
       alert("Something went wrong. Please try again later");
+      submitButton.disabled = false;
+      submitButton.innerText = "Submit";
     }
   };
 
